3485bd6b058152089ad7b849750feaba948667cf
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2021-06-15 16:50:14 -0600

    Uso de CMD / DOCKERIGNORE

--- Images/runDockerFile.sh
+++ Images/runDockerFile.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+#Este bash es para realizar el proceso de creacion de imagen, borrar, crear y correr un contenedor, 
+#y verificar si esta corriendo despues de 5 sec
+
 clear
 docker build -t apache .
 docker rm -fv apache
 docker run -d --name apache -p 80:80 apache
+sleep 5
 #clear
 docker ps
